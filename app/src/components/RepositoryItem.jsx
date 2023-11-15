539a41945e568c44d7930b819286af59968694d6
edufmass <eduardo@massolini.com.ar>
2023-11-15 11:29:24 -0300

    theme & StyledText component

--- app/src/components/RepositoryItem.jsx
+++ app/src/components/RepositoryItem.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import StyledText from "./StyledText";
 
 const RepositoryItem = (props) => (
     <View
         key={props.id}
         style={styles.container}
     >
-        <Text style={styles.strong}>Id: {props.id}</Text>
-        <Text>Name: {props.fullName}</Text>
-        <Text>Description: {props.description}</Text>
-        <Text>Language: {props.language}</Text>
-        <Text>Stars: {props.stargazersCount}</Text>
-        <Text>Forks: {props.forksCount}</Text>
-        <Text>Review: {props.reviewCount}</Text>
-        <Text>Rating: {props.ratingAverage}</Text>
+        <StyledText fontSize='subheading' fontWeight='bold' >{props.fullName}</StyledText>
+        <StyledText>{props.description}</StyledText>
+        <StyledText>{props.language}</StyledText>
+        <StyledText>Stars: {props.stargazersCount}</StyledText>
+        <StyledText>Forks: {props.forksCount}</StyledText>
+        <StyledText>Review: {props.reviewCount}</StyledText>
+        <StyledText>Rating: {props.ratingAverage}</StyledText>
     </View>
 );
 
@@ -23,11 +23,6 @@ const styles = StyleSheet.create({
     container: {
         padding: 20,
         paddingTop: 5,
-        paddingBottom: 5
-    },
-    strong: {
-        color: '#09f',
-        fontWeight: 'bold',
         paddingBottom: 5
     }
 });
