8f58f9d887b091129e8f7bc9e8f05249aed4c5bf
edufmass <eduardo@massolini.com.ar>
2023-11-27 13:11:07 -0300

    Repositories hook with apollo and graphql

--- app/src/hooks/useRepositories.js
+++ app/src/hooks/useRepositories.js
@@ -1,7 +1,14 @@
 import {useEffect, useState} from "react";
+import { useQuery } from "@apollo/client";
+ import { GET_REPOSITORIES } from "../graphql/queries";
 
 const useRepositories = () => {
-    const [repositories, setRepositories] = useState(null)
+    const { data = {}, loading, refetch } = useQuery(GET_REPOSITORIES, {
+        fetchPolicy: 'cache-and-network'
+    });
+    const { repositories = null } = data;
+
+    /*const [repositories, setRepositories] = useState(null)
 
     const fetchRepositories = async () => {
         const response = await globalThis.fetch('http://192.168.100.13:5000/api/repositories');
@@ -11,13 +18,13 @@ const useRepositories = () => {
 
     useEffect(() => {
         fetchRepositories();
-    }, []);
+    }, []);*/
 
     const repositoriesNode = repositories
         ? repositories.edges.map(edge => edge.node)
         : [];
 
-    return {repositories: repositoriesNode}
+    return { loading, repositories: repositoriesNode, refetch }
 }
 
 export default useRepositories;
--- app/src/utils/apolloClient.js
+++ app/src/utils/apolloClient.js
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 
 const createApolloClient = () => {
     return new ApolloClient({
-        uri: 'http://192.168.100.13:4000/graphql'
+        uri: 'http://192.168.100.13:4000'
     });
 }
 
